7069d3aef02cfc7eceb2b14c1276d27a2d528b90
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-03-11 00:07:55 +0900

    delete/edit

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,12 @@ import {authService} from 'fbase';
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => { // 로그인 여부
       if(user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -18,7 +20,7 @@ function App() {
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} />
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>
         ) : (
          "Initializing..."
       )}
--- src/fbase.js
+++ src/fbase.js
@@ -1,13 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD3TwJ6XKUtccygvOm2_RP01qL2CTJq6Aw",
     authDomain: "nwitter-8b5eb.firebaseapp.com",
-    projectId: "nwitter-8b5eb_URL",
+    projectId: "nwitter-8b5eb",
     storageBucket: "nwitter-8b5eb.appspot.com",
     messagingSenderId: "894573354364",
     appId: "1:894573354364:web:a0d63c18f069e5bc9058f9",
+    databaseURL: "gs://nwitter-8b5eb.appspot.com/",
 };
 
 if (!firebase.apps.length) {
@@ -16,3 +18,4 @@ if (!firebase.apps.length) {
 
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
